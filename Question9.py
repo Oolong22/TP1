26d33c38533ad61815c3a64cbffa04372679e228
oolong22 <tristansamson21@hotmail.com>
2023-09-13 21:04:08 -0400

    Completed

--- Question9.py
+++ Question9.py
@@ -1,50 +1,52 @@
 # Demander à l'utilisateur d'entrer les coefficients de l'équation quadratique
-
+a = float(input("Veuillez entrer la valeur de a (coefficient de x^2): "))
+b = float(input("Veuillez entrer la valeur de b (coefficient de x): "))
+c = float(input("Veuillez entrer la valeur de c (terme constant): "))
 # Calculer le discriminant
-
-
+delta = (b**2 - 4*a*c)
 # Vérifier si le discriminant est négatif (aucune racine réelle)
-naPasDeSolution = 
 
-if naPasDeSolution:
-    # Cette ligne de code sera exécutée si le projectile n'atteint jamais l'altitude désirée.
+if delta < 0 :
+    print("Le projectile n'atteint jamais l'altitude desiree.")
 
 else:
-    # Vérifier si le discriminant est nul (une seule racine réelle)
-    aUneSeuleSolution = 
+    # Vérifier si le discriminant est nul (une seule racine réelle
 
-    if aUneSeuleSolution:
+    if delta == 0:
         # Calculer l'instant unique où le projectile atteint l'altitude
+        x = ( (b*(-1))/(2*a))
 
 
         # Vérifier si l'instant est positif
-        estInstantPositif = 
-        if estInstantPositif:
-
-            # Afficher l'instant
+        if x > 0:
+            print("Le projectile atteint l'altitude a un seul moment precis.")
+            print("Instant de l'impact:", x)
         else:
-            # Afficher que le projectile n'atteint jamais l'altitude désirée.
+            print("Le projectile n'atteint jamais l'altitude desiree.")
 
     else:
-        # Calculer les deux instants où le projectile atteint l'altitude
+        x = (((b*(-1))-((delta)**0.5))/(2*a))
+        y = (((b*(-1))+((delta)**0.5))/(2*a))
 
-
+        print(x)
+        print(y)
         # Vérifier si les instants sont positifs
-        estInstant1PositifInstant2Negatif = 
-        estInstant2PositifInstant1Negatif = 
-        estInstant1PositifInstant2Positif = 
-        
-        if estInstant1PositifInstant2Negatif:
+       
+        if x > 0 and y < 0:
             
+            print("Instant de l'impact:", x)
+
+        elif y > 0 and x < 0:
+            
+            print("Instant de l'impact:", y)
+
             # Afficher l'instant positif
 
-        elif estInstant2PositifInstant1Negatif:
-
-            # Afficher l'instant positif
-
-        elif estInstant1PositifInstant2Positif:
+        elif x > 0 and y > 0:
+            
+            print("Instant de l'impact:", x, "et", y)
 
             # Afficher les deux instants
             
         else:
-            # Afficher que le projectile n'atteint jamais l'altitude désirée.
+            print("Le projectile n'atteint jamais l'altitude desiree.")
